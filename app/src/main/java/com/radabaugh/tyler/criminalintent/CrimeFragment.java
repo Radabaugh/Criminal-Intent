2ec39905546b556039ce17c69cac28845e82f9ca
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-16 13:07:47 -0700

    Chap 7 pg 144, Overriding onCreateView(...)

--- app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeFragment.java
@@ -2,6 +2,9 @@ package com.radabaugh.tyler.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Tyler Radabaugh on 10/16/2017.
@@ -14,5 +17,11 @@ public class CrimeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
     }
 }
