22a854b974425ab24a813afa2132eeed425a1488
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-17 13:32:25 -0700

    Chap 8 pg 164, Implementing CrimeListActivity

---
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.radabaugh.tyler.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tyler Radabaugh on 10/17/2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
