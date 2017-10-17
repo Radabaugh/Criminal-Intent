336ee4064d0190ddd89a08d847037d0bb9f0cd84
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-17 13:48:06 -0700

    Corrected error

--- app/src/main/java/com/radabaugh/tyler/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/SingleFragmentActivity.java
@@ -5,10 +5,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Tyler Radabaugh on 10/17/2017.
- */
-
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
@@ -22,7 +18,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new createFragment();
+            fragment = createFragment();
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
     }
