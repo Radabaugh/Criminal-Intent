3772ad9a216a84f10be946b9cb59d1464fca6f6a
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-29 15:38:31 -0700

    Chap 11 pg 219, Creating CrimePagerActivity

---
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,17 @@
+package com.radabaugh.tyler.criminalintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tyler Radabaugh on 10/29/2017.
+ */
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
