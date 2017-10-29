38eb6a9352c0bf64a0defa8a29040575461d2f02
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-29 15:57:19 -0700

    Chap 11 pg 221, Integrating CrimePagerActivity

--- app/src/main/java/com/radabaugh/tyler/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.radabaugh.tyler.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,20 +10,30 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Tyler Radabaugh on 10/29/2017.
  */
 
 public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID = "com.radabaugh.tyler.criminalintent.crime_id";
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
 
