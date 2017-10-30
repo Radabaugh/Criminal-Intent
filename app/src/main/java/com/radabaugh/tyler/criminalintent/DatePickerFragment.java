40655996d1b9e3c9be3b90659a534c7667f4f074
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-29 17:20:34 -0700

    Chap 12 pg 236, Passing date to DatePickerFragment

--- app/src/main/java/com/radabaugh/tyler/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/DatePickerFragment.java
@@ -6,12 +6,28 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Date;
 
 /**
  * Created by Tyler Radabaugh on 10/29/2017.
  */
 
 public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
