2352eababf909ef2b8ce62853e7b79f1e0350a98
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-31 10:24:49 -0700

    Chap 14 pg 271, Creating CrimeBaseHelper

---
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,27 @@
+package com.radabaugh.tyler.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Tyler Radabaugh on 10/31/2017.
+ */
+
+public class CrimeBaseHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        
+    }
+}
