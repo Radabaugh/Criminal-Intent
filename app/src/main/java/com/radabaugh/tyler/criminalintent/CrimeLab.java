d7bf5d8ab80616929bc04f9f069da5e50a8bc86d
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-31 11:31:55 -0700

    Chap 14 pg 278, Writing to the Db

--- app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeLab.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeLab.java
@@ -1,13 +1,17 @@
 package com.radabaugh.tyler.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.radabaugh.tyler.criminalintent.database.CrimeBaseHelper;
+import com.radabaugh.tyler.criminalintent.database.CrimeDbSchema;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static com.radabaugh.tyler.criminalintent.database.CrimeDbSchema.*;
 
 /**
  * Created by Tyler Radabaugh on 10/17/2017.
@@ -16,7 +20,6 @@ import java.util.UUID;
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
@@ -30,24 +33,27 @@ public class CrimeLab {
     private CrimeLab(Context context) {
         mContext = context.getApplicationContext();
         mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
-        mCrimes = new ArrayList<>();
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+
     }
 
     public List<Crime> getCrimes(){
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
-
         return null;
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
     }
 }
