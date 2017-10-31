789e0b29e5ea417d8a59d6b759bfba119127d0bc
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-31 10:31:29 -0700

    Chap 14 pg 272, Opening a SQLiteDatabase

--- app/src/main/java/com/radabaugh/tyler/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/database/CrimeBaseHelper.java
@@ -2,12 +2,13 @@ package com.radabaugh.tyler.criminalintent.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by Tyler Radabaugh on 10/31/2017.
  */
 
-public class CrimeBaseHelper {
+public class CrimeBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "crimeBase.db";
 
@@ -22,6 +23,6 @@ public class CrimeBaseHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        
+
     }
 }
