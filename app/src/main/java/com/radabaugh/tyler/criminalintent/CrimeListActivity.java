72c19ce1e6534b949f40336c4f7ebe90d9b6442e
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-11-10 13:58:06 -0800

    Chap 17 pg 329, Calling all callbacks!

--- app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeListActivity.java
@@ -1,5 +1,6 @@
 package com.radabaugh.tyler.criminalintent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 /**
@@ -15,5 +16,19 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        if (findViewById(R.id.detail_fragment_container) == null) {
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+            startActivity(intent);
+        } else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+        }
     }
 }
