b3c388593c42c5c5fa2a2eac0634353a24140769
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-11-10 13:48:50 -0800

    Chap 17 pg 326, Implementing CrimeListFragment.Callbacks

--- app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/radabaugh/tyler/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.radabaugh.tyler.criminalintent;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -28,8 +29,22 @@ public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
     private boolean mSubtitleVisible;
+    private Callbacks mCallbacks;
 
     private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
+    /*
+     * Required interface for hosting activities
+     */
+    public interface Callbacks {
+        void onCrimeSelected(Crime crime);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mCallbacks = (Callbacks) context;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +78,12 @@ public class CrimeListFragment extends Fragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean(SAVED_SUBTITLE_VISIBLE, mSubtitleVisible);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
     }
 
     @Override
